5f6946a8683bc93538dca407356991efaf4e68d9
HarmeshBhambra <harmeshbhambra@Harmeshs-MacBook-Air.local>
2015-12-08 22:29:43 -0600

    Default month as current month

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,27 +7,34 @@ class CalendarsController < ApplicationController
 
     @month = params[:month]
 
+    if @month.to_i >= 1
+        @mon = @month
+      else
+        @mon = Time.now.mon
+    end
+
+
     @week_1_bills = current_user.bills.select{|bill| bill.due_at.day.between?(1, 7)}
 
-    @week_1_bills_mon = @week_1_bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @week_1_bills_mon = @week_1_bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
     @week_2_bills = current_user.bills.select{|bill| bill.due_at.day.between?(8, 14)}
 
-    @week_2_bills_mon = @week_2_bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @week_2_bills_mon = @week_2_bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
     @week_3_bills = current_user.bills.select{|bill| bill.due_at.day.between?(15, 21)}
 
-    @week_3_bills_mon = @week_3_bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @week_3_bills_mon = @week_3_bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
     @week_4_bills = current_user.bills.select{|bill| bill.due_at.day.between?(22, 28)}
 
-    @week_4_bills_mon = @week_4_bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @week_4_bills_mon = @week_4_bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
     @week_5_bills = current_user.bills.select{|bill| bill.due_at.day.between?(29, 31)}
 
-    @week_5_bills_mon = @week_5_bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @week_5_bills_mon = @week_5_bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
-    @total_bills_mon = current_user.bills.select{|bill| bill.due_at.mon.to_s.include? @month.to_s}
+    @total_bills_mon = current_user.bills.select{|bill| bill.due_at.mon.to_s === @mon.to_s}
 
     @p = 0
     current_user.bills.each do |bill|
